1c6fa034ac50174390d874c412e3330ba937f1fa
zszdmzmm <1075534380@qq.com>
2023-10-31 00:38:02 +0800

    animate

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,3 +15,39 @@ const options = {
 if (document.getElementById("typed-strings")) {
     new Typed("#typed", options);
 }
+
+import 'animate.css';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import $ from 'jquery'
+window.jQuery = window.$ = $
+
+// extend jQuery to add a function that does it animate css
+$.fn.extend({
+    animateCss: function(animationName, callback) {
+        var animationEnd = (function(el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
+
+        this.addClass('animate__animated ' + 'animate__' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animate__animated ' + 'animate__' + animationName);
+
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    },
+});

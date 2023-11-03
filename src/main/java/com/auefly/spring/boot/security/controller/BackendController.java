0e754c4e91bfebb81bde326ae3eca8fd0ab885fd
zszdmzmm <1075534380@qq.com>
2023-11-04 07:56:45 +0800

    method security...
    https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html#:~:text=Method%20Security-,Method%20Security,-%F0%9F%94%84

--- src/main/java/com/auefly/spring/boot/security/controller/BackendController.java
+++ src/main/java/com/auefly/spring/boot/security/controller/BackendController.java
@@ -3,6 +3,8 @@ package com.auefly.spring.boot.security.controller;
 import com.auefly.spring.boot.security.config.BackendProperties;
 import com.auefly.spring.boot.security.config.Menus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.List;
 
 @Controller
+@EnableMethodSecurity
 public class BackendController {
     @Autowired
     private BackendProperties backendProperties;
@@ -20,16 +23,19 @@ public class BackendController {
     }
 
     @GetMapping("/admin/dashboard")
+    @PreAuthorize("hasRole('admin')")
     public String adminDashboard() {
         return "backend/dashboard";
     }
 
     @GetMapping("/admin/users")
+    @PreAuthorize("hasRole('admin')")
     public String adminUsers() {
         return "backend/dashboard-users";
     }
 
     @GetMapping("/admin/posts")
+    @PreAuthorize("hasRole('admin')")
     public String adminPosts() {
         return "backend/dashboard-posts";
     }

4f7dd4a434843690a5d14ef1b4c5de9fc2eab1eb
zszdmzmm <1075534380@qq.com>
2023-11-26 05:08:27 +0800

    update section

--- src/test/java/com/auefly/spring/boot/security/controller/backend/SectionControllerTest.java
+++ src/test/java/com/auefly/spring/boot/security/controller/backend/SectionControllerTest.java
@@ -2,11 +2,9 @@ package com.auefly.spring.boot.security.controller.backend;
 
 import com.auefly.spring.boot.security.controller.WithMockUserForAdminBaseTest;
 import com.auefly.spring.boot.security.entity.Collection;
-import com.auefly.spring.boot.security.entity.Lecture;
 import com.auefly.spring.boot.security.entity.Section;
 import com.auefly.spring.boot.security.entity.User;
 import com.auefly.spring.boot.security.repository.CollectionRepository;
-import com.auefly.spring.boot.security.repository.LectureRepository;
 import com.auefly.spring.boot.security.repository.SectionRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -26,8 +24,6 @@ class SectionControllerTest extends WithMockUserForAdminBaseTest {
     CollectionRepository collectionRepository;
     @Autowired
     SectionRepository sectionRepository;
-    @Autowired
-    LectureRepository lectureRepository;
 
     @Test
     @DisplayName("新增Session")
@@ -108,49 +104,6 @@ class SectionControllerTest extends WithMockUserForAdminBaseTest {
         collectionRepository.delete(collection);
 
         Assertions.assertTrue(sectionRepository.findById(section.getId()).isEmpty());
-        Assertions.assertTrue(collectionRepository.findById(collection.getId()).isEmpty());
-    }
-
-
-    @Test
-    @DisplayName("删除section")
-    void destroy() throws Exception {
-        String collectionTitle = UUID.randomUUID().toString();
-        Collection collection = new Collection();
-        collection.setTitle(collectionTitle);
-        collection.setSlug(UUID.randomUUID().toString());
-        collection.setType("doc");
-        collection.setUser(new User(1L));
-        collectionRepository.save(collection);
-
-        String sectionTitle = UUID.randomUUID().toString();
-        Section section = new Section();
-        section.setTitle(sectionTitle);
-        section.setCollection(new Collection(collection.getId()));
-        sectionRepository.save(section);
-
-        String lectureTitle = UUID.randomUUID().toString();
-        Lecture lecture = new Lecture();
-        lecture.setTitle(lectureTitle);
-        lecture.setSection(new Section(section.getId()));
-        lecture.setCollection(new Collection(collection.getId()));
-        lectureRepository.save(lecture);
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/admin/sections/destroy/" + section.getId()))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-        ;
-
-        lectureRepository.delete(lecture);
-        Optional<Lecture> deletedLecture = lectureRepository.findById(lecture.getId());
-        Assertions.assertTrue(deletedLecture.isEmpty());
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/admin/sections/destroy/" + section.getId()))
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin/collections/edit/" + collection.getId()))
-        ;
-        Assertions.assertTrue(sectionRepository.findById(section.getId()).isEmpty());
-
-
-        collectionRepository.delete(collection);
         Assertions.assertTrue(collectionRepository.findById(collection.getId()).isEmpty());
     }
 }

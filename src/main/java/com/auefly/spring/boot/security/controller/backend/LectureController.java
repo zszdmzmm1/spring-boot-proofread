38ca3e41feac1034c6d59d8bd8bb2651df98e0fc
zszdmzmm <1075534380@qq.com>
2023-11-27 06:54:32 +0800

    auto save blocks on create lecture

--- src/main/java/com/auefly/spring/boot/security/controller/backend/LectureController.java
+++ src/main/java/com/auefly/spring/boot/security/controller/backend/LectureController.java
@@ -7,6 +7,7 @@ import com.auefly.spring.boot.security.service.LectureService;
 import com.auefly.spring.boot.security.service.SectionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,13 +38,20 @@ public class LectureController {
         return "backend/lecture/create";
     }
 
+    @Value("${custom.block.separator}")
+    String blockSeparator;
+
     @PostMapping("")
-    String store(@Valid @ModelAttribute("lecture") LectureDto lectureDto,
-                 BindingResult result) {
+    @Transactional
+    public String store(@Valid @ModelAttribute("lecture") LectureDto lectureDto,
+                        BindingResult result) {
         if(result.hasErrors()) {
             return "backend/lecture/create";
         }
-        lectureService.save(lectureDto);
+        Lecture lecture = lectureService.save(lectureDto);
+        if (lectureDto.getContent()!=null && lectureDto.getContent().contains(blockSeparator)) {
+            lectureService.saveBlocks(lecture.getId(), lectureDto);
+        }
         return "redirect:/admin/collections/edit/" + lectureDto.getCollection_id();
     }
 
--- src/test/java/com/auefly/spring/boot/security/controller/backend/LectureControllerTest.java
+++ src/test/java/com/auefly/spring/boot/security/controller/backend/LectureControllerTest.java
@@ -2,23 +2,25 @@ package com.auefly.spring.boot.security.controller.backend;
 
 
 import com.auefly.spring.boot.security.controller.WithMockUserForAdminBaseTest;
-import com.auefly.spring.boot.security.entity.Collection;
-import com.auefly.spring.boot.security.entity.Lecture;
-import com.auefly.spring.boot.security.entity.Section;
-import com.auefly.spring.boot.security.entity.User;
+import com.auefly.spring.boot.security.entity.*;
+import com.auefly.spring.boot.security.repository.BlockRepository;
 import com.auefly.spring.boot.security.repository.CollectionRepository;
 import com.auefly.spring.boot.security.repository.LectureRepository;
 import com.auefly.spring.boot.security.repository.SectionRepository;
+import com.auefly.spring.boot.security.service.BlockService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class LectureControllerTest extends WithMockUserForAdminBaseTest {
     @Autowired
@@ -27,6 +29,8 @@ public class LectureControllerTest extends WithMockUserForAdminBaseTest {
     SectionRepository sectionRepository;
     @Autowired
     LectureRepository lectureRepository;
+    @Autowired
+    BlockService blockService;
 
     @Test
     @DisplayName("新增一个lecture")
@@ -128,5 +132,110 @@ public class LectureControllerTest extends WithMockUserForAdminBaseTest {
         Assertions.assertTrue(lectureRepository.findById(lecture.getId()).isEmpty());
         Assertions.assertTrue(sectionRepository.findById(section.getId()).isEmpty());
         Assertions.assertTrue(collectionRepository.findById(collection.getId()).isEmpty());
+    }
+
+
+    @Test
+    @DisplayName("删除lectures")
+    void destroy() throws Exception {
+        String collectionTitle = UUID.randomUUID().toString();
+        Collection collection = new Collection();
+        collection.setTitle(collectionTitle);
+        collection.setSlug(UUID.randomUUID().toString());
+        collection.setType("doc");
+        collection.setUser(new User(1L));
+        collectionRepository.save(collection);
+
+        String sectionTitle = UUID.randomUUID().toString();
+        Section section = new Section();
+        section.setTitle(sectionTitle);
+        section.setCollection(new Collection(collection.getId()));
+        sectionRepository.save(section);
+
+        String lectureTitle = UUID.randomUUID().toString();
+        Lecture lecture = new Lecture();
+        lecture.setTitle(lectureTitle);
+        lecture.setSection(new Section(section.getId()));
+        lecture.setCollection(new Collection(collection.getId()));
+        lectureRepository.save(lecture);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/admin/lectures/destroy/" + lecture.getId()))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin/collections/edit/" + collection.getId()))
+        ;
+
+        Optional<Lecture> byId = lectureRepository.findById(lecture.getId());
+        Assertions.assertTrue(byId.isEmpty());
+
+        sectionRepository.delete(section);
+        Assertions.assertTrue(sectionRepository.findById(section.getId()).isEmpty());
+        collectionRepository.delete(collection);
+        Assertions.assertTrue(collectionRepository.findById(collection.getId()).isEmpty());
+    }
+
+    @Test
+    @DisplayName("自动保存blocks")
+    void autoSaveBlocksOnCreateLecture(@Autowired Environment environment) throws Exception {
+        Collection collection = new Collection();
+        collection.setTitle(UUID.randomUUID().toString());
+        collection.setSlug(UUID.randomUUID().toString());
+        collection.setType("doc");
+        collection.setUser(new User(1L));
+        collectionRepository.save(collection);
+
+        Section section = new Section();
+        section.setTitle(UUID.randomUUID().toString());
+        section.setCollection(new Collection(collection.getId()));
+        sectionRepository.save(section);
+
+
+        String lectureTitle = "title-" + UUID.randomUUID();
+        String blockSeparator = environment.getProperty("custom.block.separator");
+        String lectureContent = """
+                ## block-1-title
+                                                
+                block-1-content
+                                                
+                %s
+                                                
+                ## block-2-title
+                                                
+                block-2-content
+                                                
+                %s
+                                                
+                ## block-3-title
+                                                
+                block-3-content
+                """.formatted(blockSeparator, blockSeparator);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/admin/lectures")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("id", "")
+                        .param("title", lectureTitle)
+                        .param("content", lectureContent)
+                        .param("sortOrder", "0")
+                        .param("section_id", section.getId().toString())
+                        .param("collection_id", collection.getId().toString())
+                )
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin/collections/edit/" + collection.getId()))
+        ;
+
+        Optional<Lecture> le = lectureRepository.findFirstByTitle(lectureTitle);
+        Assertions.assertTrue(le.isPresent());
+
+        Lecture lecture = le.get();
+        List<Block> blocks = lecture.getBlocks();
+        Assertions.assertEquals(3, blocks.size());
+        blockService.destroyAllById(blocks.stream().map(Block::getId).collect(Collectors.toList()));
+
+        lectureRepository.deleteById(lecture.getId());
+
+        Optional<Section> se = sectionRepository.findById(section.getId());
+        Assertions.assertTrue(se.isPresent());
+        sectionRepository.delete(se.get());
+
+        Optional<Collection> co = collectionRepository.findById(collection.getId());
+        Assertions.assertTrue(co.isPresent());
+        collectionRepository.delete(co.get());
     }
 }
